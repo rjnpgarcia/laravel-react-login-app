91bc3cb698695a5f8925e3d28c7c6d28ca06de3e
rjnpgarcia <102405417+rjnpgarcia@users.noreply.github.com>
2023-01-05 04:40:28 +0800

    2nd commit

--- react-login-app/src/views/Signup.jsx
+++ react-login-app/src/views/Signup.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useRef } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
@@ -8,7 +9,7 @@ function Signup() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
-
+    const [errors, setErrors] = useState(null);
     const { setUser, setToken } = useStateContext();
 
     const onSubmit = (ev) => {
@@ -19,16 +20,17 @@ function Signup() {
             password: passwordRef.current.value,
             password_confirmation: passwordConfirmationRef.current.value,
         };
+        console.log(payload);
         axiosClient
             .post("/signup", payload)
-            .then((data) => {
+            .then(({ data }) => {
                 setUser(data.user);
                 setToken(data.token);
             })
             .catch((err) => {
                 const response = err.response;
                 if (response && response.status === 422) {
-                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
                 }
             });
     };
@@ -38,6 +40,13 @@ function Signup() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free</h1>
+                    {errors && (
+                        <div className="alert">
+                            {Object.keys(errors).map((key) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
                     <input ref={nameRef} type="text" placeholder="Full Name" />
                     <input ref={emailRef} type="email" placeholder="Email" />
                     <input
